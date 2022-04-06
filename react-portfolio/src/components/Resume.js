bb3575cd0e94fa5fdd93a02f3ff8f51e62b667b9
Hamza Syed <sahamzashah19@gmail.com>
2022-04-06 18:09:31 +0500

    first commit

---
+++ react-portfolio/src/components/Resume.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+export default class Resume extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="resume">
+        {/* <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
+          <div className="nine columns main-col">
+            {resumeData.education &&
+              resumeData.education.map((item) => {
+                return (
+                  <div className="row item">
+                    <div className="twelve columns">
+                      <h3>{item.UniversityName}</h3>
+                      <p className="info">
+                        {item.specialization}
+                        <span>&bull;</span>{" "}
+                        <em className="date">
+                          {item.MonthOfPassing} {item.YearOfPassing}
+                        </em>
+                      </p>
+                      <p>{item.Achievements}</p>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div> */}
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Projects</span>
+            </h1>
+          </div>
+          <div className="nine columns main-col">
+            {resumeData.work &&
+              resumeData.work.map((item) => {
+                return (
+                  <div className="row item">
+                    <div className="twelve columns">
+                      <h3>
+                        <span>&bull;</span>
+                        <span> Portfolio</span>
+                      </h3>
+                      <p className="info">
+                        <span>React Js</span>
+                        <span>&bull;</span>{" "}
+                        <em className="date">
+                          <span>April 2022</span>
+                        </em>
+                      </p>
+                      <p>{}</p>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
+          </div>
+          <div className="nine columns main-col">
+            <p>{resumeData.skillsDescription}</p>
+            <div className="bars">
+              <ul className="skills">
+                {resumeData.skills &&
+                  resumeData.skills.map((item) => {
+                    return (
+                      <li>
+                        <span className={`bar-expand ${"30"}%`}></span>
+                        <em>{item.skillname}</em>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ react-portfolio/src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+  imagebaseurl: "https://ibb.co/SRsFCh4",
+  name: "MSxSLASHER",
+  role: "Programmer",
+  linkedinId: "https://www.linkedin.com/in/syed-ali-hamza-shah-92a343205/",
+  instagramid: "Your instagramid",
+  roleDescription:
+    "I like to learn and create applications in various parts of software development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/syed-ali-hamza-shah-92a343205/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/hamza-140",
+      className: "fa fa-github",
+    },
+    {
+      name: "instagram",
+      url: "https://instagram.com/dementors_15",
+      className: "fa fa-instagram",
+    },
+  ],
+  aboutme: "I am in infinite while loop with coding.",
+  address: "Pakistan",
+  website: "https://github.com/hamza-140",
+  education: [
+    {
+      UniversityName: "Hochschule Fulda",
+      specialization: "Machine Learning",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2022",
+      Achievements: "Some Achievements",
+    },
+    {
+      UniversityName: "Some University",
+      specialization: "Some specialization",
+      MonthOfPassing: "Jan",
+      YearOfPassing: "2018",
+      Achievements: "Some Achievements",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "FLUTTER",
+    },
+    {
+      skillname: "C++",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+export default resumeData;
